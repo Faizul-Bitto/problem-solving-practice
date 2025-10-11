cf6a9b39b85c31ac482ad547136c64186b8e7e5a
Faizul Islam Bhuiyan (Bitto) <fibhuiyanbitto06@gmail.com>
2025-10-12 00:28:56 +0600

    feat(C/15-I-Welcome-for-you-with-Conditions-(ICPC-Assiut-Community-Contest-CODEFORCES)): add solution for ICPC Assiut welcome with conditions problem
    Add C program solution that compares two numbers and prints "Yes" if the first is greater or equal, otherwise "No". Includes input/output test files and compiled executable.

---
+++ C/15-I-Welcome-for-you-with-Conditions-(ICPC-Assiut-Community-Contest-CODEFORCES)/welcome-for-you-with-conditions.c
@@ -0,0 +1,23 @@
+// https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/I
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+    int A, B;
+    scanf("%d %d", &A, &B);
+
+    if (A >= B)
+    {
+        printf("Yes");
+    }
+    else
+    {
+        printf("No");
+    }
+
+    return 0;
+}
