3c4b66c0f138637cb065b6ae159aa879ed43907d
Faizul Islam Bhuiyan (Bitto) <fibhuiyanbitto06@gmail.com>
2025-10-12 00:47:34 +0600

    fix(C/21-C-Simple-Calculator-(ICPC-Assiut-Community-Contest-CODEFORCES)): correct scanf format specifier in simple calculator
    The scanf function was incorrectly using three format specifiers for two variables, which could lead to undefined behavior. Removed the extra format specifier to match the actual number of variables being read.

--- C/21-C-Simple-Calculator-(ICPC-Assiut-Community-Contest-CODEFORCES)/simple-calculator.c
+++ C/21-C-Simple-Calculator-(ICPC-Assiut-Community-Contest-CODEFORCES)/simple-calculator.c
@@ -8,7 +8,7 @@
 int main()
 {
     long long int X, Y;
-    scanf("%lld %lld %lld", &X, &Y);
+    scanf("%lld %lld", &X, &Y);
 
     printf("%lld + %lld = %lld\n%lld * %lld = %lld\n%lld - %lld = %lld", X, Y, (X + Y), X, Y, (X * Y), X, Y, (X - Y));
 
