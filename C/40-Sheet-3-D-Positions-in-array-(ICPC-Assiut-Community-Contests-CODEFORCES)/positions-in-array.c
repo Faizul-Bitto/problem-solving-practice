791b475acf7cb48c13f30ca0fcc049e220678e32
Faizul-Bitto <fibhuiyanbitto06@gmail.com>
2025-10-16 00:17:27 +0600

    feat(40-Sheet-3-D-Positions-in-array-(ICPC-Assiut-Community-Contests-CODEFORCES)): implement positions in array program
    - Add main program to read an integer array and print values less than or equal to 10
    - Include input and output test files for verification
    - Provide problem link in source code comments for reference

---
+++ C/40-Sheet-3-D-Positions-in-array-(ICPC-Assiut-Community-Contests-CODEFORCES)/positions-in-array.c
@@ -0,0 +1,29 @@
+// https://codeforces.com/group/MWSDmqGsZm/contest/219774/problem/D
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+    int N;
+    scanf("%d", &N);
+
+    int A[N];
+
+    for (int i = 0; i < N; i++)
+    {
+        scanf("%d", &A[i]);
+    }
+
+    for (int i = 0; i < N; i++)
+    {
+        if (A[i] <= 10)
+        {
+            printf("A[%d] = %d \n", i, A[i]);
+        }
+    }
+
+    return 0;
+}
