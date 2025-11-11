a3a6beda58ff501e4b4472d4246a28f91db54753
Faizul-Bitto <fibhuiyanbitto06@gmail.com>
2025-11-11 23:09:49 +0600

    feat(104-change-it-function): implement program to modify array and print elements
    - Add main program to change the last element of an integer array to 100 and print the array
    - Include input and output files for testing the functionality
    - Add compiled executable for the program

---
+++ C/104-change-it-function/change-it-function.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int changeIt(int A[], int N)
+{
+    A[N - 1] = 100;
+
+    for (int i = 0; i < N; i++)
+    {
+        printf("%d ", A[i]);
+    }
+}
+
+int main()
+{
+    int N;
+    scanf("%d", &N);
+
+    int A[N];
+    for (int i = 0; i < N; i++)
+    {
+        scanf("%d", &A[i]);
+    }
+
+    changeIt(A, N);
+
+    return 0;
+}
