67b2e4f17dff8ca2f796f2b5b95a2ce6ea31e189
Faizul-Bitto <fibhuiyanbitto06@gmail.com>
2025-11-18 19:32:09 +0600

    feat(01-return-dynamic-array): add dynamic array program files
    - Include C++ implementation for returning a dynamic array based on user input
    - Add input file with test case for the dynamic array
    - Add output file with expected results for the test case
    - Include compiled executable for the program

---
+++ C++/Random/01-return-dynamic-array/01-return-dynamic-array.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int *get_array(int N)
+{
+    int *a = new int[N];
+
+    for (int i = 0; i < N; i++)
+    {
+        cin >> a[i];
+    }
+
+    return a;
+}
+
+int main()
+{
+    int N;
+    cin >> N;
+
+    int *x = get_array(N);
+
+    for (int i = 0; i < N; i++)
+    {
+        cout << x[i] << " ";
+    }
+
+    return 0;
+}
