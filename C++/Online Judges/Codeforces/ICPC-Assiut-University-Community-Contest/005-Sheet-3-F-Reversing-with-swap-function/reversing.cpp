0cf5b5c8b69f5aa25cec6bc58ff55ff39773f5a6
Faizul-Bitto <fibhuiyanbitto06@gmail.com>
2025-11-19 02:15:56 +0600

    feat(005-Sheet-3-F-Reversing-with-swap-function): add reversing program files
    - Introduce C++ implementation for reversing an array using the swap function
    - Add input file with test case for the reversing function
    - Include output file with expected results for the test case
    - Add compiled executable for the program

---
+++ C++/Online Judges/Codeforces/ICPC-Assiut-University-Community-Contest/005-Sheet-3-F-Reversing-with-swap-function/reversing.cpp
@@ -0,0 +1,35 @@
+// https://codeforces.com/group/MWSDmqGsZm/contest/219774/problem/F
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+
+    int A[N];
+
+    for (int i = 0; i < N; i++)
+    {
+        cin >> A[i];
+    }
+
+    int i = 0;
+    int j = N - 1;
+
+    while (i < j)
+    {
+        swap(A[i], A[j]);
+
+        i++;
+        j--;
+    }
+
+    for (int i = 0; i < N; i++)
+    {
+        cout << A[i] << " ";
+    }
+
+    return 0;
+}
