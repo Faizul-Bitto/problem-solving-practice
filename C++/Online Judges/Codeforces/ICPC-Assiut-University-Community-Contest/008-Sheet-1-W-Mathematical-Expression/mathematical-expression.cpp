f331e51d3121a352d8615b20dc73471420d62c7f
Faizul-Bitto <fibhuiyanbitto06@gmail.com>
2025-11-20 01:23:00 +0600

    feat(008-Sheet-1-W-Mathematical-Expression): add mathematical expression evaluation program files
    - Introduce C++ implementation for evaluating mathematical expressions
    - Add input file with a test case for the expression
    - Include output file with expected results for the test case
    - Add compiled executable for the program

---
+++ C++/Online Judges/Codeforces/ICPC-Assiut-University-Community-Contest/008-Sheet-1-W-Mathematical-Expression/mathematical-expression.cpp
@@ -0,0 +1,35 @@
+// https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/W
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int A, B, C;
+    char S, Q;
+    cin >> A >> S >> B >> Q >> C;
+
+    if (S == '+')
+    {
+        if (A + B == C)
+            cout << "Yes";
+        else
+            cout << A + B;
+    }
+    else if (S == '-')
+    {
+        if (A - B == C)
+            cout << "Yes";
+        else
+            cout << A - B;
+    }
+    else if (S == '*')
+    {
+        if (A * B == C)
+            cout << "Yes";
+        else
+            cout << A * B;
+    }
+
+    return 0;
+}
