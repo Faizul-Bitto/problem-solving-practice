4c2b64ea216af2c0fdc34344c5108c4002e870b7
Faizul-Bitto <fibhuiyanbitto06@gmail.com>
2025-11-25 00:00:50 +0600

    feat(011-Sheet-3-M-Replace-MinMax): add program to replace minimum and maximum values in an array
    - Introduce C++ implementation for swapping the minimum and maximum values in an array
    - Add input file with test cases for the program
    - Include output file with expected results for the test cases
    - Add compiled executable for the program

---
+++ C++/Online Judges/Codeforces/ICPC-Assiut-University-Community-Contest/011-Sheet-3-M-Replace-MinMax/replace-minxmax.cpp
@@ -0,0 +1,47 @@
+// https://codeforces.com/group/MWSDmqGsZm/contest/219774/problem/M
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+
+    int A[N];
+    for (int i = 0; i < N; i++)
+    {
+        cin >> A[i];
+    }
+
+    int minimumNumber = INT_MAX;
+    int maximumNumber = INT_MIN;
+    int minimumNumberIndex = INT_MAX;
+    int maximumNumberIndex = INT_MIN;
+
+    for (int i = 0; i < N; i++)
+    {
+        if (A[i] < minimumNumber)
+        {
+            minimumNumber = A[i];
+            minimumNumberIndex = i;
+        }
+
+        if (A[i] > maximumNumber)
+        {
+            maximumNumber = A[i];
+            maximumNumberIndex = i;
+        }
+    }
+
+    int temp = A[minimumNumberIndex];
+    A[minimumNumberIndex] = A[maximumNumberIndex];
+    A[maximumNumberIndex] = temp;
+
+    for (int i = 0; i < N; i++)
+    {
+        cout << A[i] << " ";
+    }
+
+    return 0;
+}
